bde44dcaf16c07070875001b5af8eb34ccfa9139
Gerontius Leo L <71941953+gerontiusleo@users.noreply.github.com>
2021-04-08 10:10:10 +0530

    Add files via upload

---
+++ second project-Copy1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+list1 =[10,9,-4,4,-7]
+for i in list1:
+    if i >= 0:
+        print (i)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
